1b80808e18f3f90da1918937e827718bd7d28bcf
Crcs1225 <117502889+Crcs1225@users.noreply.github.com>
2025-08-26 21:14:16 +0800

    rm -"unnecesary files"

--- src/utils/CodingSetup.tsx
+++
@@ -1,7 +0,0 @@
-import { useGLTF } from "@react-three/drei";
-import { Group } from "three";
-
-export default function CodingSetup() {
-  const { scene } = useGLTF("/desk.glb") as { scene: Group };
-  return <primitive object={scene} position={[0, -2.5, -2]} scale={0.5} />;
-}
